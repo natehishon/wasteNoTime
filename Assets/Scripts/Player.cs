5bfc2016667dca52980bd3f32305ff6ab9534cec
nhishon <nhishon@luc.edu>
2019-03-14 11:55:08 -0500

    init commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    
+    }
+
+    private void Move()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0);
+
+        }
+    }
+}
